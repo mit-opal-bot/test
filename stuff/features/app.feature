402988aa932227f8f24b4c64ca4e1ff586596133
Justin Anderson <jander@mit.edu>
2018-04-02 14:47:47 -0400

    Failing test

--- stuff/features/app.feature
+++ stuff/features/app.feature
@@ -3,3 +3,7 @@ Feature: Hello world
   Scenario: Hello world
      When we request a page
      Then we should get hello world
+
+  Scenario: Failure
+    When we want a test to fail
+    Then it does
--- stuff/features/steps/steps.py
+++ stuff/features/steps/steps.py
@@ -10,3 +10,11 @@ def step_impl(context):
 @then("we should get hello world")
 def step_impl(context):
     assert context.response.content == b"Hello World!", context.response.content
+    
+@When("we want a test to fail")
+def step_impl(context):
+    pass
+
+@then("it does")
+def step_impl(context):
+    assert False == True
