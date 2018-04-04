0ab78deccee161d2da9c230ccf32d30fbe328ac8
Justin Anderson <jander@mit.edu>
2018-04-04 11:40:10 -0400

    Some but not all

--- stuff/features/steps/steps.py
+++ stuff/features/steps/steps.py
@@ -1,20 +1,20 @@
 from behave import *
 import requests
 
-@when("we request a page")
+@when("we request a page") # pylint: disable=undefined-variable
 def step_impl(context): # pylint: disable=function-redefined
     url = "http://app:5000/"
     context.response = requests.get(url)
 
 
-@then("we should get hello world")
+@then("we should get hello world") # pylint: disable=undefined-variable
 def step_impl(context): # pylint: disable=function-redefined
     assert context.response.content == b"Hello World!", context.response.content
     
-@When("we want a test to fail")
+@When("we want a test to fail") # pylint: disable=undefined-variable
 def step_impl(context): # pylint: disable=function-redefined
     assert False == True
 
-@When("we want a test to succeed")
-def step_impl(context): # pylint: disable=function-redefined
+@When("we want a test to succeed") # pylint: disable=undefined-variable
+def step_impl(context):
     assert True == True
