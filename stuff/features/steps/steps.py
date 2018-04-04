1a3dfb65ab500489f8a7273433cba7b752f3fb5c
Justin Anderson <jander@mit.edu>
2018-04-04 11:32:14 -0400

    Suppress Pylint issues

--- stuff/features/steps/steps.py
+++ stuff/features/steps/steps.py
@@ -2,19 +2,19 @@ from behave import *
 import requests
 
 @when("we request a page")
-def step_impl(context):
+def step_impl(context): # pylint: disable=function-redefined
     url = "http://app:5000/"
     context.response = requests.get(url)
 
 
 @then("we should get hello world")
-def step_impl(context):
+def step_impl(context): # pylint: disable=function-redefined
     assert context.response.content == b"Hello World!", context.response.content
     
 @When("we want a test to fail")
-def step_impl(context):
+def step_impl(context): # pylint: disable=function-redefined
     assert False == True
 
 @When("we want a test to succeed")
-def step_impl(context):
+def step_impl(context): # pylint: disable=function-redefined
     assert True == True
